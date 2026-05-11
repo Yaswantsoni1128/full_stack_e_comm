3256c4cec24c64936ed81b59bd059dbef37ae384
Yaswant <yaswantsoni2005@gmail.com>
2026-05-11 23:51:28 +0530

    Fix: Cloudinary utility and error handling
    Corrected the usage of cloudinary.uploader methods, added error rejection, and ensured options are passed properly.

---
+++ Backend/utils/cloudinary.js
@@ -0,0 +1,43 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.SECRET_KEY,
+});
+
+const cloudinaryUploadImg = async (fileToUploads) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      fileToUploads,
+      { resource_type: "auto" },
+      (error, result) => {
+        if (error) return reject(error);
+        resolve({
+          url: result.secure_url,
+          asset_id: result.asset_id,
+          public_id: result.public_id,
+        });
+      }
+    );
+  });
+};
+
+const cloudinaryDeleteImg = async (fileToDelete) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.destroy(
+      fileToDelete,
+      { resource_type: "auto" },
+      (error, result) => {
+        if (error) return reject(error);
+        resolve({
+          url: result.secure_url,
+          asset_id: result.asset_id,
+          public_id: result.public_id,
+        });
+      }
+    );
+  });
+};
+
+module.exports = { cloudinaryUploadImg, cloudinaryDeleteImg };
